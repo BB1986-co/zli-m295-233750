22cda14e67d4882f8b63a0d2a318c41839d796ff
BB1986-co <143395483+BB1986-co@users.noreply.github.com>
2023-12-14 15:35:52 +0000

    final commit Tag2

---
+++ aufgabe-4-1/getTimeTimezone.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/now?tz=', (request, response) => {
+  
+    const now = new Date().toLocaleTimeString()   
+    response.send(`Es ist gerade: ${now}`)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
