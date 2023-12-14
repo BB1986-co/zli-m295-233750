10d2f8f905e82d8f44c32bd66cc15907bc35d384
BB1986-co <143395483+BB1986-co@users.noreply.github.com>
2023-12-14 10:38:57 +0000

    update teilaufgabe 3-2 gelöst

--- aufgabe-3-2/weather.js
+++ aufgabe-3-2/weather.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+app.get('/:postleitzahl', (request, response) => {
+    const plz = request.params.postleitzahl;
+     
+  response.send(plz);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
