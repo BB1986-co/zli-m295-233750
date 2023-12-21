ff0e8e899ea7b5324603f923a5dd4a94be9153dd
BB1986-co <143395483+BB1986-co@users.noreply.github.com>
2023-12-21 15:37:27 +0000

    chore create 7-2

--- aufgabe-7-1/sessionImp.js
+++ aufgabe-7-1/sessionImp.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const session = require('express-session');
-const app = express();
+// npm install express-session
 
-app.use(express.json());
+const express = require('express')
+const session = require('express-session')
+const app = express()
+
 app.use(session({
-    secret: 'supersecret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {}
-}));
+  secret: 'supersecret',
+	resave: false,
+	saveUninitialized: true,
+  cookie: {}
+}))
 
-app.post('/name'){
-    
-}
+app.get('/', function (request, response, _) {
+  // request.session is the object that holds the information of this specific session
+  request.session.views = (request.session.views || 0) + 1
+  console.log(request.session)
+
+  response.end(request.session.views + ' views')
+})
+
+app.listen(3000)
